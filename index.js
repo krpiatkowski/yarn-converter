670877a22276e2e700f14702bb5cb4f02ccdcefe
Krzysztof Piatkowski <krzysztof.piatkowski@lego.com>
2021-02-21 15:30:59 +0100

    Added code

---
+++ index.js
@@ -0,0 +1,17 @@
+function calculate() {
+    const expected = Number.parseInt(document.getElementById('expected').value);
+    const fromG = Number.parseInt(document.getElementById('from-g').value);
+    const fromM = Number.parseInt(document.getElementById('from-m').value);
+    const toG = Number.parseInt(document.getElementById('to-g').value);
+    const toM = Number.parseInt(document.getElementById('to-m').value);
+    const resultNode =  document.getElementById('result');
+
+    if([expected, fromG, fromM, toG, toM].includes(NaN)) {
+        resultNode.innerText = "";
+        return;
+    }
+
+    const result = (fromG/fromM) / (toG/toM) * expected;
+    resultNode.innerText = result + "g";
+
+}
